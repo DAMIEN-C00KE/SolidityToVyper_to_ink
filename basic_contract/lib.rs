29fc148a56ecaee4036fd3617e6a5f8648f91773
HCT <110128281+DAMIEN-C00KE@users.noreply.github.com>
2023-09-13 18:50:22 +0300

    Update lib.rs

--- basic_contract/lib.rs
+++ basic_contract/lib.rs
@@ -26,34 +26,6 @@
 // Updated due to bool_changed being declared as mutable but then
 // reassigned with immutable variables.
 //
-// SOLIDITY CONTRACT
-// - Added parentheses to the setBool if statement
-// - Added emit to UpdatedBool(newBool) event 
-/*
-contract BasicContract {
-    bool private _theBool;
-    event UpdatedBool(bool indexed _theBool);
-
-    constructor(bool theBool_) {
-        require(theBool_ == true, "theBool_ must start as true");
-
-        _theBool = theBool_;
-    }
-
-    function setBool(bool newBool) public returns (bool boolChanged) {
-        if (_theBool == newBool) {
-               boolChanged = false;
-        } else {
-            boolChanged = true;
-        }
-
-        _theBool = newBool;
-        // emit event
-        emit UpdatedBool(newBool);
-    }
-}
-*/
-
 #![cfg_attr(not(feature = "std"), no_std)]
 
 #[ink::contract]
