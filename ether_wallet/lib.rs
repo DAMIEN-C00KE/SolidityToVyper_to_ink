80f5a1b369fdb27a0f829718ef8757b44a1cbf86
HCT <110128281+DAMIEN-C00KE@users.noreply.github.com>
2023-09-14 12:27:19 +0300

    Move Solidity comment to own file

--- ether_wallet/lib.rs
+++ ether_wallet/lib.rs
@@ -1,30 +1,3 @@
-// SOLIDITY
-// Contract taken from https://solidity-by-example.org/app/ether-wallet/
-/*
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-contract EtherWallet {
-    address payable public owner;
-
-    constructor() {
-        owner = payable(msg.sender);
-    }
-
-    receive() external payable {}
-
-    function withdraw(uint _amount) external {
-        require(msg.sender == owner, "caller is not owner");
-        payable(msg.sender).transfer(_amount);
-    }
-
-    function getBalance() external view returns (uint) {
-        return address(this).balance;
-    }
-}
-*/
-
-// ink! Rust
 #![cfg_attr(not(feature = "std"), no_std)]
 
 #[ink::contract]
